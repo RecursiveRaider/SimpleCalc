6e575a5d48a765933a9807e7bf0123a797b04dff
Swaraj Kumar <153834409+RecursiveRaider@users.noreply.github.com>
2024-10-23 02:50:17 +0530

    Updated addtion calc.py

--- calc.py
+++ calc.py
@@ -2,3 +2,8 @@ operand1 = float(input("Enter the first number: "))
 operand2 = float(input("Enter the second number: "))
 operator = input("Enter operator (+, -, *, /): ")
 
+#person 1 ka code
+if operator == '+':
+    result = operand1 + operand2
+    print("Result:", result)
+
